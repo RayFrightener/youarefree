63df40c6b56ca50f181da8c0c39ca150f62838c9
Magician <pranshuchawla19@gmail.com>
2025-11-10 14:04:24 -0600

    feat: add speed analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import SessionProviderWrapper from "../components/SessionProviderWrapper";
 import { ModalProvider } from "../context/ModalContext";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Unbound",
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
           </ModalProvider>
         </SessionProviderWrapper>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
